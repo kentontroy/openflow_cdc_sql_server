7ede0c6f56412d63369b1b741439c95a2d5cda40
Kenton Troy <kentontroy@yahoo.com>
2026-01-25 23:52:25 -0500

    Update cdc-enable.sql

--- sql/cdc-enable.sql
+++ sql/cdc-enable.sql
@@ -1,17 +1,38 @@
-ALTER TABLE SalesLT.Product DISABLE CHANGE_TRACKING;
-ALTER DATABASE "sql-db-0408286" SET CHANGE_TRACKING = OFF;
+CREATE OR ALTER PROCEDURE dbo.enable_cdc_for_table
+  @source_schema sysname,
+  @source_name   sysname,
+  @supports_net_changes bit = 0,
+  @role_name sysname = NULL
+AS
+BEGIN
+  SET NOCOUNT ON;
 
-SELECT * FROM sys.change_tracking_databases WHERE database_id = DB_ID('sql-db-0408286');
-SELECT t.name, c.is_track_columns_updated_on 
-FROM sys.change_tracking_tables c JOIN sys.tables t ON c.object_id = t.object_id WHERE t.name = 'sql-db-0408286';
+  -- Enable CDC at DB level if needed
+  IF EXISTS (SELECT 1 FROM sys.databases WHERE name = DB_NAME() AND is_cdc_enabled = 1)
+    PRINT 'CDC already enabled for database ' + QUOTENAME(DB_NAME()) + '.';
+  ELSE
+  BEGIN
+    PRINT 'Enabling CDC for database ' + QUOTENAME(DB_NAME()) + '...';
+    EXEC sys.sp_cdc_enable_db;
+  END
 
-EXEC sys.sp_cdc_enable_db
+  DECLARE @obj_id int = OBJECT_ID(QUOTENAME(@source_schema) + N'.' + QUOTENAME(@source_name));
+  IF @obj_id IS NULL
+    THROW 50000, 'Source table not found in current database.', 1;
 
-EXEC sys.sp_cdc_enable_table
-    @source_schema = N'SalesLT',
-    @source_name   = N'Product',
-    @role_name     = NULL,
-    @supports_net_changes = 0;
+  -- Enable CDC for table if not already enabled
+  IF EXISTS (SELECT 1 FROM cdc.change_tables WHERE source_object_id = @obj_id)
+    PRINT 'CDC already enabled for ' + QUOTENAME(@source_schema) + '.' + QUOTENAME(@source_name) + '.';
+  ELSE
+  BEGIN
+    PRINT 'Enabling CDC for ' + QUOTENAME(@source_schema) + '.' + QUOTENAME(@source_name) + '...';
+    EXEC sys.sp_cdc_enable_table
+      @source_schema        = @source_schema,
+      @source_name          = @source_name,
+      @role_name            = @role_name,
+      @supports_net_changes = @supports_net_changes;
+  END
 
-SELECT * FROM cdc.change_tables;
+  SELECT * FROM cdc.change_tables ORDER BY capture_instance;
+END;
 GO
