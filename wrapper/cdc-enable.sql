2cf83b3dee4ce12f0034fd55a8526d25b917d86b
Kenton Troy <kentontroy@yahoo.com>
2026-02-08 21:34:08 -0500

    Update CDC enable command for test_sales_events

--- wrapper/cdc-enable.sql
+++ wrapper/cdc-enable.sql
@@ -1,6 +1,5 @@
-EXEC sys.sp_cdc_enable_table
+EXEC dbo.enable_cdc_for_table 
   @source_schema = N'dbo',
-  @source_name   = N'cdc_test_sales_events',
-  @role_name     = NULL,
-  @supports_net_changes = 0;
+  @source_name = N'test_sales_events',
+  @supports_net_changes = 1;
 GO
